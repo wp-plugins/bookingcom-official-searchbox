7e26cd780ddd5c8d4fba5aa7e35897ceafcdda14
SP - Booking.com <SP - Booking.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-13 08:26:44 +0000

    Push entire working copy to repo. Push of the ver 1.0 of Booking.com Official Searchbox Plugin for Wordpress
    git-svn-id: https://plugins.svn.wordpress.org/bookingcom-official-searchbox/trunk@913149 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/bos_searchbox.php
@@ -0,0 +1,59 @@
+
+<div id="flexi_searchbox" style="<?php echo  $bgcolor ? 'background-color:'. $bgcolor .';' : ''; echo  $textcolor ? 'color:'. $textcolor .';' : '' ; echo $widget_width ? 'width:'. $widget_width . 'px;' : '' ;  ?>" >
+
+    <div id="b_searchboxInc">
+        <h3 class="search-box-title-1"  style="<?php echo  $textcolor ? 'color:' . $textcolor . ';' : 'color: #003580;' ; ?>"><?php echo $maintitle ? $maintitle : __( 'Need a hotel?' , BOS_TEXT_DOMAIN ) ; ?></h3>
+         <form id="b_frm" action="<?php echo $target_page ; ?>" method="get" target="_blank" onsubmit="return sp.validation.validSearch();">
+            <div id="searchBox_error_msg" class="b_error b_external_searchbox" style="display: none;"></div>
+            <div id="b_frmInner">
+                
+                <input type="hidden" name="error_url" value="<?php echo $target_page ; ?>?aid=<?php echo !empty( $options[ 'aid' ] ) ? $options[ 'aid' ] : BOS_DEFAULT_AID ; ?>;" />
+                <input type="hidden" name="si" value="ai,co,ci,re,di" />
+                <input type="hidden" name="label" value="wp-searchbox-widget-<?php echo !empty( $options[ 'aid' ] ) ? $options[ 'aid' ] : BOS_DEFAULT_AID ; ?>" />                   
+                <input type="hidden" name="aid" value="<?php echo !empty( $options[ 'aid' ] ) ? $options[ 'aid' ] : BOS_DEFAULT_AID ; ?>" />
+                <input type="hidden" name="utm_campaign" value="search_box" /> 
+                <input type="hidden" name="utm_medium" value="sp" /> 
+                <input type="hidden" name="utm_term" value="wp-widget-<?php echo !empty( $options[ 'aid' ] ) ? $options[ 'aid' ] : BOS_DEFAULT_AID ; ?>" /> 
+
+                <div id="b_searchDest">
+                    
+                    <?php if ( $destination ) { ?>
+                        <input type="text" id="b_destination" name="ss" value="<?php echo $destination ; ?>" />
+                    <?php } else { ?>
+                        <input type="text" id="b_destination" name="ss" placeholder="<?php _e('e.g. city, region, district or specific hotel' , BOS_TEXT_DOMAIN ) ; ?>" title="<?php _e( 'e.g. city, region, district or specific hotel' , BOS_TEXT_DOMAIN ) ;?>" />
+                    <?php } ?>
+                    
+                </div><!-- #b_searchDest -->
+
+                <div id="searchBox_dates_error_msg" class="b_error b_external_searchbox" style="display: none ;"></div>
+
+
+                 <?php  
+                    //Include function generate date
+                    include_once BOS_INC_PLUGIN_DIR . '/bos_date_array.php';
+                                        
+                    echo bos_dateSelector( $month_format, $calendar, $checkin, $checkout, $textcolor );  
+                ?>
+
+                <div class="avail">
+                    <input type="hidden" value="on" name="do_availability_check" />
+                </div><!-- .b_submitButton_wrapper-->
+
+                <div class="b_submitButton_wrapper">
+                    <input class="b_submitButton" type="submit" value="<?php  echo $submit ? $submit : __( 'search' , BOS_TEXT_DOMAIN ) ; ?>" style="<?php echo  $submit_bgcolor ? 'background-color:'. $submit_bgcolor . ';' : '' ; echo  $submit_textcolor ? 'color:'. $submit_textcolor . ';' : '' ; echo $submit_bordercolor ? 'border-color:' . $submit_bordercolor . ';' : '' ;  ?>"/>
+                </div><!-- .b_submitButton_wrapper-->
+                
+                <div id="b_logo" <?php echo  $logopos ? 'style="text-align:' . $logopos . ';"' : '' ;  ?> ><img src="<?php echo BOS_IMG_PLUGIN_DIR .'/booking_logotype_' . $logodim . '.png' ; ?>" alt="Booking.com"></div>                
+                <!-- #b_logo" -->                        
+                
+
+            </div><!-- #b_frmInner -->
+         </form>
+    </div><!-- #b_searchboxInc -->
+
+
+    <div id="b_calendarPopup" class="b_popup">
+        <div id="b_calendarInner" class="b_popupInner "></div>
+    </div>
+
+</div><!-- #flexi_searchbox -->
